1a1a90e8076c409681d2e2e8ccf575aac7b8d802
TianW_hp_ubuntu <wuyucan1998@live.com>
2019-01-12 14:34:24 -0500

    part2-print created

---
+++ part2.py
@@ -0,0 +1,52 @@
+
+import pprint
+
+def part1():
+    def SortKey(val):
+        return val['ProductNumber']
+
+    # In[30]:
+    products = {}
+    inputFile = open("programming2019-master/2a.in")
+    length = int(inputFile.readline().split(' ')[1][:-1])
+    inputStr = inputFile.read()
+    inputStrList = inputStr.split('\n')
+    inputList = []
+    for i in inputStrList[:length]:
+        inputList.append(i.lstrip('(').rstrip(')').split(','))
+    inputListofDict = []
+    for i in inputList:
+        inputListofDict.append \
+            ({'Location': (int(i[0]), int(i[1])), 'ProductNumber': int(i[2]), "Weight": float(i[3]), "Qty": 1})
+    # pprint.pprint(inputListofDict)
+    # In[31]:
+    inputListofDict.sort(key=SortKey)
+    i = 1
+    while i < len(inputListofDict):
+        if inputListofDict[i]["ProductNumber"] == inputListofDict[i - 1]["ProductNumber"]:
+            del inputListofDict[i]
+            inputListofDict[i - 1]["Qty"] += 1
+        else:
+            i += 1
+    return inputListofDict
+
+
+def part2_path(productList):
+    itemMap = {}
+    for productEntryA in productList:
+        locationA = productEntryA.get('Location')
+        productNumberA = productEntryA.get('ProductNumber')
+        itemMap[(productNumberA, 0)] = \
+            -max(abs(0 - locationA[0]), abs(0 - locationA[1]))
+        itemMap[(0, productNumberA)] = \
+            -max(abs(0 - locationA[0]), abs(0 - locationA[1]))
+        for productEntryB in productList:
+            locationB = productEntryB.get('Location')
+            productNumberB = productEntryB.get('ProductNumber')
+            itemMap[(productNumberA, productNumberB)] = \
+                -max(abs(locationA[0] - locationB[0]), abs(locationA[1] - locationB[1]))
+    return itemMap
+
+if __name__ == '__main__':
+    # pprint.pprint(part1())
+    pprint.pprint(part2_path(part1()))
---
+++ part3-bfs.py
@@ -0,0 +1,10 @@
+
+def part3_bfs(locA, locB, mapDict):
+    path = [locA]
+    i = 0
+    while i < len(path):
+        if 
+
+# def part3_path():
+
+if __name__ == '__main__':
